9f3cb5ee2c6ee83f22a884188ab9dd00f9a51348
Bulat <reaktive.the.good@gmail.com>
2021-12-12 21:31:59 +0300

    Test

---
+++ Yandex/VC_Demo/VC_Demo/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  VC_Demo
+//
+//  Created by Булат Хасанов on 28.11.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func ButtonClosed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
